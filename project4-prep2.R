b3b68e0f30cf9e29d770c5166149627a5547c1ac
Jonathan Moreno-Medina <jrm87@duke.edu>
2022-11-02 16:17:13 -0500

    upload data and code for ML model

---
+++ project4-prep2.R
@@ -0,0 +1,32 @@
+
+#DATA ANALYSIS
+library(dplyr)
+library(here)
+# Load data:
+
+atlas_datacommons_final<-read.csv(here("data","atlas_datacommons_tract.csv"))
+
+# Some of the variables in the data create problems (allegedly)
+atlas_datacommons_final<-atlas_datacommons_final%>%
+  select(-c('Value.Percent_Person_WithDiabetes_2018'))
+
+# Show variables:
+names(atlas_datacommons_final)
+
+# Drop variable that gives some problems:
+
+
+# set up seed:
+set.seed(12345)
+
+#Creating an id variable to divide data into test and training
+atlas_datacommons_final$id <- 1:nrow(atlas_datacommons_final)
+
+#use 70% of dataset as training set and 30% as test set
+train <- atlas_datacommons_final %>% dplyr::sample_frac(0.70)
+test  <- dplyr::anti_join(atlas_datacommons_final, train, by = 'id')
+
+# Save training and testing dataset:
+saveRDS(train, here("data","atlas_train.rds"))
+saveRDS(test, here("data","atlas_test.rds"))
+
